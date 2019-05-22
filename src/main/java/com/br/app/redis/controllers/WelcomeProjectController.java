98eed658598f703140355208b7d713f709fb3b6f
Luiz Felipe do Carmo  Durante <luiz.durante@triadsystems.com.br>
2019-05-22 13:50:40 -0300

    mvn

---
+++ src/main/java/com/br/app/redis/controllers/WelcomeProjectController.java
@@ -0,0 +1,16 @@
+//package com.br.app.redis.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@CrossOrigin(origins = "*")
+//public class WelcomeProjectController {
+//
+////	@RequestMapping("/")
+////	public String listaConvidados() {
+////		System.out.println("entrou");
+////		return "redirect:/index";
+////	}
+//}
