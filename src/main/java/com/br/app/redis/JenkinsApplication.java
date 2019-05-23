758e5e3d26f362e0b79f50543bc9cbdac52c8c25
Luiz Felipe do Carmo  Durante <luiz.durante@triadsystems.com.br>
2019-05-23 12:35:18 -0300

    Vai monstro

--- src/main/java/com/br/app/redis/JenkinsApplication.java
+++ src/main/java/com/br/app/redis/JenkinsApplication.java
@@ -3,6 +3,10 @@ package com.br.app.redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Luiz
+ *
+ */
 @SpringBootApplication
 public class JenkinsApplication {
 
