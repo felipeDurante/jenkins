d2dbce6b78f8f6dcdc923c2c7089c51c788aa0c1
Luiz Felipe do Carmo  Durante <luiz.durante@triadsystems.com.br>
2019-05-24 13:56:13 -0300

    agoravai

--- src/main/java/com/br/app/redis/ServletInitializer.java
+++ src/main/java/com/br/app/redis/ServletInitializer.java
@@ -3,6 +3,10 @@ package com.br.app.redis;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author Luiz
+ *
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
